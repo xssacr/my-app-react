e69831256dd6b7d8ac91f5b4c439d3673f7a42fe
小赛赛爱吃肉 <xssacr@aliyun.com>
2020-04-23 22:16:33 +0800

    配置 按需引入 and-mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Index from './views/Index'
 function App() {
   return (
     <div className="App">
-      
+      <Index></Index>
     </div>
   );
 }
---
+++ src/views/Index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Button } from "antd-mobile";
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">default</Button>
+      </div>
+    );
+  }
+}
